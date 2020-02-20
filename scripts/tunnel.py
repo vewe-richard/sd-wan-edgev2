7fee6da71051b3980e73f5050b49dd47b0ea6770
gaokaoyimin <jiang.j.qian@hotmail.com>
2020-02-20 14:18:57 +0800

    update tunnel.py script for the new design

--- scripts/tunnel.py
+++ scripts/tunnel.py
@@ -7,10 +7,21 @@
 #    set environment, PYTHONPATH=/home/richard/PycharmProjects/sd-wan-edgev2, CONFIGFILE=config.json
 #    and working directory, /home/richard/PycharmProjects/sd-wan-edgev2
 # 2. run in shell:
-#    sudo su
-#    export PYTHONPATH="$PWD" &&  python3 scripts/tunnel.py -s -p 5555
-#    export PYTHONPATH="$PWD" &&  python3 scripts/tunnel.py -c 127.0.0.1 -p 5555 -l 10.139.37.2
-#   export PYTHONPATH="$PWD" &&  python3 scripts/tunnel.py -d -p 5555
+# sudo su
+# python3 sd-wan-env/ns4tunnels.py
+#
+# export PYTHONPATH="$PWD" && python3 scripts/tunnel.py -s -p 5555 -l 10.139.27.1 -n main
+# export PYTHONPATH="$PWD" && python3 scripts/tunnel.py -c 10.129.101.100 -p 5555 -l 10.139.27.3 -n n101
+# ip netns exec n101 ping 10.139.27.1
+#
+# export PYTHONPATH="$PWD" && python3 scripts/tunnel.py -s -p 55556 -l 10.139.47.1 -n main
+# export PYTHONPATH="$PWD" && python3 scripts/tunnel.py -c 10.129.101.100 -p 55556 -l 10.139.47.3 -n n101
+# ip netns exec n101 ping 10.139.47.1
+#
+# export PYTHONPATH="$PWD" && python3 scripts/tunnel.py -s -p 33333 -l 10.139.27.1 -n main
+# export PYTHONPATH="$PWD" && python3 scripts/tunnel.py -c 10.129.102.100 -p 33333 -l 10.139.27.8 -n n102
+# ip netns exec n102 ping 10.139.27.3
+
 from getopt import getopt
 import sys
 import socket
@@ -18,58 +29,23 @@ import os
 import subprocess
 from edgeutils import utils
 
+def doreport(ret, out, err):
+    try:
+        from edgeutils import utils
+        report = utils.reportactionresult(os.environ["SN"], os.environ["ACTIONID"], os.environ["ACTIONTYPE"],
+                                      ret, out, err)
+        utils.http_post(os.environ["SMS"], os.environ["SMSPORT"], "/north/actionresult/", report)
+    except Exception as e:
+        print("returncode: ", ret)
+        print("stdout: ", out)
+        print("stderr: ", err)
+
 def usage():
-    print("python3 scripts/tunnel.py [-s|-d|-c serverip] [-p port] [-l locaip] [-h|--help]", file=sys.stderr)
-    pass
-
-def server(port):
-    # check parameters
-    # if port is available, if port is used, it will raise exception socket.error
-    cmdline = "/usr/bin/python3 " + os.getcwd() + "/scripts/tunnels/simpletunnel.py -s -p " + str(port)
-
-    # install service
-    with open("./scripts/tunnels/simpletun.service") as f:
-        tmp = f.read().replace("{GITROOT}", os.getcwd())
-        serviceFile = tmp.replace("{CMDLINE}", cmdline)
-    svcname = "simpletun.s." + str(port) + ".service"
-    print("install service: ", svcname)
-    subprocess.run(["systemctl", "disable", svcname])
-    subprocess.run(["systemctl", "stop", svcname])
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(("127.0.0.1", port))
-    with open("/lib/systemd/system/"+svcname, "w") as f:
-        f.write(serviceFile)
-    subprocess.run(["systemctl", "daemon-reload"])
-    subprocess.run(["systemctl", "start", svcname])
-    subprocess.run(["systemctl", "enable", svcname])
-    pass
-
-def client(ip, port, localip):
-    #TODO: check if localip is belong to "10.139.37.1/24"
-    iprange = "10.139.37."
-    slen = len(iprange)
-    if len(localip) < slen or localip[:slen] not in iprange:
-        print("Wrong local ip, is not in range", file=sys.stderr)
-        sys.exit(-1)
-    cmdline = "/usr/bin/python3 " + os.getcwd() + "/scripts/tunnels/simpletunnel.py -c " + ip + " -p " + str(port) + " -l " + localip
-
-    # install service
-    with open("./scripts/tunnels/simpletun.service") as f:
-        tmp = f.read().replace("{GITROOT}", os.getcwd())
-        serviceFile = tmp.replace("{CMDLINE}", cmdline)
-    svcname = "simpletun.c." + str(port) + ".service"
-    print("install service: ", svcname)
-    with open("/lib/systemd/system/"+svcname, "w") as f:
-        f.write(serviceFile)
-    subprocess.run(["systemctl", "disable", svcname])
-    subprocess.run(["systemctl", "stop", svcname])
-    subprocess.run(["systemctl", "daemon-reload"])
-    subprocess.run(["systemctl", "start", svcname])
-    subprocess.run(["systemctl", "enable", svcname])
+    print("python3 scripts/tunnel.py [-s|-d|-c serverip] [-p port] [-l locaip] [-v] [-n namespace] [-h|--help]", file=sys.stderr)
     pass
 
 def delete(port):
-    svcnames = ["simpletun.s."+str(port)+".service", "simpletun.c."+str(port)+".service"]
+    svcnames = ["simpletun.s."+port+".service", "simpletun.c."+port+".service"]
     for svc in svcnames:
         sp = subprocess.run(["systemctl", "status", svc])
         if sp.returncode != 0:
@@ -83,52 +59,67 @@ def delete(port):
     pass
 
 
+
+def install(svcname, cmdline):
+    # install service
+    with open("./scripts/tunnels/simpletun.service") as f:
+        tmp = f.read().replace("{GITROOT}", os.getcwd())
+        serviceFile = tmp.replace("{CMDLINE}", cmdline)
+    print("install service: ", svcname)
+    subprocess.run(["systemctl", "disable", svcname])
+    subprocess.run(["systemctl", "stop", svcname])
+    with open("/lib/systemd/system/"+svcname, "w") as f:
+        f.write(serviceFile)
+    subprocess.run(["systemctl", "daemon-reload"])
+    subprocess.run(["systemctl", "start", svcname])
+    subprocess.run(["systemctl", "enable", svcname])
+    pass
+
 if __name__ == "__main__":
-    opts, args = getopt(sys.argv[1:], "hsdc:p:l:", ["help"])
-    port = None
+    opts, args = getopt(sys.argv[1:], "hvsdc:p:l:n:", ["help"])
+    port = ""
     localip = None
+    verbose = ""
+    ns = ""
+    remove = False
 
     for o, v in opts:
         if o in "-h" or o in "--help":
             usage()
             sys.exit(-1)
         elif o in "-p":
-            port = int(v)
-            pass
+            port = v
         elif o in "-l":
-            localip = v
+            localip = " -l " + v + " "
+        elif o in "-v":
+            verbose = "-v"
+        elif o in "-n":
+            ns = "/sbin/ip netns exec " + v + " "
+        elif o in "-d":
+            remove = True
             pass
 
-    if port == None:
-        print("Port is not provided", file=sys.stderr)
+    int(port)
+    if remove:
+        delete(port)
+        doreport(0, "OK", "")
+        sys.exit(0)
+
+    if localip == None:
+        print("Localip is not provided", file=sys.stderr)
         usage()
         sys.exit(-1)
 
+    cmdline = None
     for o, v in opts:
         if o in "-s":
             print("simpletunnel server, port: ", port)
-            server(port)
-            pass
+            svcname = "simpletun.s." + port + ".service"
+            cmdline = ns + "/usr/bin/python3 " + os.getcwd() + "/scripts/tunnels/simpletunnel.py -s -p " + port + localip + verbose
         elif o in '-c':
             ip = v
-            if localip == None:
-                usage()
-                sys.exit(-1)
-            client(ip, port, localip)
-            pass
-        elif o in "-d":
-            delete(port)
-            pass
-
-    try:
-        os.environ["SN"]
-    except:
-        # in test environment, just exit, do not report
-        print("test environment", file=sys.stderr)
-        sys.exit(-1)
-
-    report = utils.reportactionresult(os.environ["SN"], os.environ["ACTIONID"], os.environ["ACTIONTYPE"],
-                                          0, "OK", "")
-    utils.http_post(os.environ["SMS"], os.environ["SMSPORT"], "/north/actionresult/", report)
-    sys.exit(0)
-    pass
+            svcname = "simpletun.c." + port + ".service"
+            cmdline = ns + "/usr/bin/python3 " + os.getcwd() + "/scripts/tunnels/simpletunnel.py -c " + ip + " -p " + port + localip + verbose
+    if not cmdline is None:
+        install(svcname, cmdline)
+        doreport(0, "OK", "")
